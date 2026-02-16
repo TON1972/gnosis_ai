72c56f1fb707f20e534be471e12875f0e40b968d
Alessandro Bueno <pep.contatos01@gmail.com>
2026-02-16 18:33:18 -0300

    vercel

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "../../server/routers";
-import { createContext } from "../../server/_core/context";
+import { appRouter } from "../../server/routers.js";
+import { createContext } from "../../server/_core/context.js";
 
 export const runtime = "nodejs";
 
