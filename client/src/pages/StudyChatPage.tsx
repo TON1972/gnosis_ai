fdd63fa1ab85238314f33ba89983fdcec0c4c5d6
alebbueno <pep.contatos01@gmail.com>
2026-07-06 14:39:45 -0300

    correcao

--- client/src/pages/StudyChatPage.tsx
+++ client/src/pages/StudyChatPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
 import { useRoute, Link, useLocation } from "wouter";
 import { trpc } from "@/lib/trpc";
 import { Button } from "@/components/ui/button";
